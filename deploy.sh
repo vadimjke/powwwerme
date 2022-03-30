3348a59c6c930d57bcdc33d0529207fe379ad6b3
Vadimjke <it.vadik@gmail.com>
2022-03-30 19:45:07 +0000

    Git CD

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,19 @@
 
 # activate maintenance mode
 php artisan down
+
 # update source code
 git pull
+
 # update PHP dependencies
 composer install --no-interaction --no-dev --prefer-dist
 # --no-interaction Do not ask any interactive question
 # --no-dev  Disables installation of require-dev packages.
 # --prefer-dist  Forces installation from package dist even for dev versions.
+
 # update database
-php artisan migrate --force
+#php artisan migrate --force
+
 # --force  Required to run when in production.
 # stop maintenance mode
 php artisan up

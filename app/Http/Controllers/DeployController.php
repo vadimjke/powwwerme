b13439e093942de1b1306856f9c107856e4340d9
Vadimjke <it.vadik@gmail.com>
2022-03-30 23:19:02 +0300

    Git CD

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -16,7 +16,7 @@ class DeployController extends Controller
       $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
       if (hash_equals($githubHash, $localHash)) {
             $root_path = base_path();
-            $process = new Process(['deploy.sh']);
+            $process = new Process(['./deploy.sh']);
             $process->setWorkingDirectory($root_path);
             $process->run(function ($type, $buffer) {
                 echo $buffer;

dac0556c6737b727f2594a70003200f1ae71f0c1
Vadimjke <it.vadik@gmail.com>
2022-03-30 22:18:26 +0300

    Git CD tested

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::statamic('authors', 'author.index');
 Route::statamic('authors/{handle}', 'author.show');
+
+// Git CD
 Route::post('/deploy/', 'DeployController@deploy');
